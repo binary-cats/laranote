c581c86c325f3a7353287ec7c50664075e549887
Cyrill Kalita <cyrill.kalita@gmail.com>
2019-03-10 19:21:32 -0500

    Create base tests

---
+++ tests/Unit/ProviderTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace BinaryCats\Laranote\Tests\Unit;
+
+use BinaryCats\Laranote\Tests\TestCase;
+
+class ProviderTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testProviderIsInitializable()
+    {
+        $this->assertTrue(true);
+    }
+}
